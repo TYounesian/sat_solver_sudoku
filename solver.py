86748032d0b080c18f9be908c67a2709b8d76973
Mark Adamik <dorteel@gmail.com>
2021-11-20 15:26:54 +0100

    ADD: New heuristics + added heuristics to a dict

--- solver.py
+++ solver.py
@@ -1,14 +1,8 @@
 import itertools
 from copy import deepcopy
 import time
+import sys
 
-
-
-rules = "etc/sudoku-rules-9x9.txt"
-sudokuSource = "etc/damnhard.sdk.txt"
-#sudokuSource = "etc/1000 sudokus.txt"
-
-branches = 0
 # =================================== SETUP - READING FILES ===================================
 
 def getRules(rulePath):
@@ -30,7 +24,7 @@ def getSudokuFromFile(sudokuPath):
 
 # =================================== DPLL Algorithm ===================================
 
-def dpll(cnf):
+def dpll(cnf, heuristic):
     global branches
     cls = []                                                            # Collects the unit clauses from each iter.
     while True:
@@ -45,13 +39,23 @@ def dpll(cnf):
         solution.extend(list(set(cls)))                 # If the set is empty we satisfied it all
         return list(set(solution))                      # And the unit clauses can be added to the solution
     if not all(cnf): return False                       # If a clause is empty it cannot be satisfied
-    p = heuristics_JWOS(cnf)                            # Choose a heuristic
-    branches += 1
-    if dpll(deepcopy(cnf) + [[p]]) or dpll(deepcopy(cnf) + [[-p]]):     # Either branch should be correct
+    p = heuristic(cnf)                                  # Choose a heuristic
+    branches += 1                                       # Either branch should be correct
+    if dpll(deepcopy(cnf) + [[p]], heuristic) or dpll(deepcopy(cnf) + [[-p]], heuristic):
         solution.extend(list(set(cls)))                                 # Add the solution to the set
         return list(set(solution))                                      # Return solutions
     else: return False
 
+
+def solver_complete(cnf, heuristic):
+    cnfloop = cnf[:]                                    # ------------- Needs checking for tautologies -------------
+    for clause in cnfloop:                              # Looping trough a copy of the input and
+        for a, b in itertools.combinations(clause, 2):  # checking if any of the paris in the clauses sum to 0
+            if (a + b) == 0: cnf.remove(clause)         # ----------------------------------------------------------
+    return dpll(cnf, heuristic)                                    # ------- Beginning real algorithm
+
+
+# =================================== Heuristics ===================================
 
 def heuristics_JWOS(cnf):                               # One-sided Jeroslow-Wang Heuristics
     weights = {}
@@ -72,10 +76,13 @@ def heuristics_JWTS(cnf):                               # Two-sided Jeroslow-Wan
     return max(weights, key=weights.get)                # Return the most frequent predicate
 
 
-
-def heuristics_DLCS(cnf):
-    flat_cnf = [abs(p) for c in cnf for p in c]    # Flatten the cnf
+def heuristics_DLCS(cnf):                               # Dynamic Largest Combined Sum
+    flat_cnf = [abs(p) for c in cnf for p in c]         # Flatten the cnf
     return max(flat_cnf, key=flat_cnf.count)            # Return the most frequent predicate
+
+
+def heuristics_FIRST(cnf):                              # First predicate we encounter
+    return cnf[0][0]                                    # Return the first predicate from the first clause
 
 
 def heuristics_DLCS_2(cnf):
@@ -89,44 +96,50 @@ def heuristics_DLCS_2(cnf):
     return max(weights, key=weights.get)            # Return the most frequent predicate
 
 
-def solver_complete(cnf):
-    cnfloop = cnf[:]                                    # ------------- Needs checking for tautologies -------------
-    for clause in cnfloop:                              # Looping trough a copy of the input and
-        for a, b in itertools.combinations(clause, 2):  # checking if any of the paris in the clauses sum to 0
-            if (a + b) == 0: cnf.remove(clause)         # ----------------------------------------------------------
-    return dpll(cnf)                                    # ------- Beginning real algorithm
-
-
 heuristics = {
     'JWOS': heuristics_JWOS,
     'JWTS': heuristics_JWTS,
     'DLCS': heuristics_DLCS,
     'DLCS_2': heuristics_DLCS_2,
+    'FIRST': heuristics_FIRST
 }
 
 #  =================================== MAIN PROGRAM ===================================
-totaltime = 0
-totalbranches = 0
 
-puzzles = getSudokuFromFile(sudokuSource)
-loops = 10
-totalsolutions = 0
-for i in range(loops):
-    solution = []
-    puzzle = puzzles[i]
-    start = time.time()
-    solutions = solver_complete(getRules(rules) + puzzle)
-    timetaken = time.time() - start
-    totaltime += timetaken
-    totalbranches += branches
-    totalsolutions += len(solutions)
-    #  =================================== SOLUTION CHECK ==================================
-    print("\n====== Puzzle No. {} ======\n".format(i))
-    print("Puzzle: ", puzzle)
-    print("Solution: ", solutions)
-    print("Number of solutions", len(solutions))
-    print("Branching needed: ", branches)
-    print("Time taken", timetaken)
-print("*"*100)
-print("Average time taken: {}\nAverage branches needed: {}".format(totaltime/loops, totalbranches/loops))
-print(totalsolutions/loops)
+if __name__ == '__main__':
+
+    rules = "etc/sudoku-rules-9x9.txt"
+    # sudokuSource = "etc/damnhard.sdk.txt"
+    sudokuSource = "etc/1000 sudokus.txt"
+
+    branches = 0
+
+    totaltime = 0
+    totalbranches = 0
+
+    h = 'FIRST'
+
+    heuristic = heuristics[h]
+
+    puzzles = getSudokuFromFile(sudokuSource)
+    loops = 10
+    totalsolutions = 0
+    for i in range(loops):
+        solution = []
+        puzzle = puzzles[i]
+        start = time.time()
+        solutions = solver_complete(getRules(rules) + puzzle, heuristic)
+        timetaken = time.time() - start
+        totaltime += timetaken
+        totalbranches += branches
+        totalsolutions += len(solutions)
+        #  =================================== SOLUTION CHECK ==================================
+        print("\n====== Puzzle No. {} ======\n".format(i))
+        print("Puzzle: ", puzzle)
+        print("Solution: ", solutions)
+        print("Number of solutions", len(solutions))
+        print("Branching needed: ", branches)
+        print("Time taken", timetaken)
+    print("\n{}\nAverage time taken: {}\nAverage branches needed: {}".format("=" * 100, totaltime / loops,
+                                                                             totalbranches / loops))
+    print(totalsolutions / loops)
